a38215998ef3c6a0ee9ab4c8857d5c4c1db0598b
Robson Banda <138120031+robiebnd@users.noreply.github.com>
2026-08-10 11:39:49 +0200

    Add Purchase Requisition create validation

--- src/main/java/com/digipals/wms/purchaserequisition/dto/CreatePurchaseRequisitionRequest.java
+++ src/main/java/com/digipals/wms/purchaserequisition/dto/CreatePurchaseRequisitionRequest.java
@@ -1,15 +1,18 @@
 package com.digipals.wms.purchaserequisition.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.util.UUID;
 
-
 @Data
 public class CreatePurchaseRequisitionRequest {
 
+    @NotNull(message = "Warehouse is required")
     private UUID warehouseId;
 
+    @NotBlank(message = "Department is required")
     private String department;
 
     private String remarks;
