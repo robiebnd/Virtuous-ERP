d5110dc18c7681ed0983af21ed8f41365dd43bbb
Robson Banda <138120031+robiebnd@users.noreply.github.com>
2026-08-10 12:44:47 +0200

    Require supplier when creating PO from approved requisition

--- src/main/java/com/digipals/wms/purchaseorders/dto/CreatePurchaseOrderRequest.java
+++ src/main/java/com/digipals/wms/purchaseorders/dto/CreatePurchaseOrderRequest.java
@@ -1,6 +1,5 @@
 package com.digipals.wms.purchaseorders.dto;
 
-import com.digipals.wms.purchaseorders.entity.ProcurementSource;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
@@ -9,13 +8,6 @@ import java.util.UUID;
 @Data
 public class CreatePurchaseOrderRequest {
 
-    @NotNull
+    @NotNull(message = "Supplier is required")
     private UUID supplierId;
-
-    @NotNull
-    private UUID warehouseId;
-
-    private UUID purchaseRequisitionId;
-
-    private ProcurementSource source;
 }
