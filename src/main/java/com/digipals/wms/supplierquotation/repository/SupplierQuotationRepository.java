f5e7bfb90a345f273733844a6ca2f66cc0dd2d70
Robson Banda <138120031+robiebnd@users.noreply.github.com>
2026-08-14 15:33:39 +0200

    add supplier quotation repository

---
+++ src/main/java/com/digipals/wms/supplierquotation/repository/SupplierQuotationRepository.java
@@ -0,0 +1,12 @@
+package com.digipals.wms.supplierquotation.repository;
+
+import com.digipals.wms.supplierquotation.entity.SupplierQuotation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface SupplierQuotationRepository extends JpaRepository<SupplierQuotation, UUID> {
+
+    List<SupplierQuotation> findByPurchaseRequisitionIdOrderByCreatedAtDesc(UUID purchaseRequisitionId);
+}
