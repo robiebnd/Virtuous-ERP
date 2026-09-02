47e475bbcba38af583269ae981da7d20099cf31e
Robson Banda <138120031+robiebnd@users.noreply.github.com>
2026-09-02 22:27:17 +0200

    Align SAP sales order response with OData payload

--- src/main/java/com/digipals/wms/salesorder/sap/SapSalesOrderResponse.java
+++ src/main/java/com/digipals/wms/salesorder/sap/SapSalesOrderResponse.java
@@ -1,6 +1,24 @@
 package com.digipals.wms.salesorder.sap;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record SapSalesOrderResponse(
+        @JsonProperty("d") Data data,
         String salesOrderNumber
 ) {
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public record Data(
+            @JsonProperty("SalesOrder") String salesOrderNumber
+    ) {
+    }
+
+    public String resolvedSalesOrderNumber() {
+        if (salesOrderNumber != null && !salesOrderNumber.isBlank()) {
+            return salesOrderNumber;
+        }
+        return data == null ? null : data.salesOrderNumber();
+    }
 }
