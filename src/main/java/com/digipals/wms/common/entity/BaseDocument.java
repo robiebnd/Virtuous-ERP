a1b70b89a79c6b71002e30fd6c17dd9397968a3b
Robson Banda <138120031+robiebnd@users.noreply.github.com>
2026-09-03 14:10:10 +0200

    Fix BaseDocument Lombok super builder

--- src/main/java/com/digipals/wms/common/entity/BaseDocument.java
+++ src/main/java/com/digipals/wms/common/entity/BaseDocument.java
@@ -4,9 +4,11 @@ import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 @Getter
 @Setter
+@SuperBuilder
 @MappedSuperclass
 public abstract class BaseDocument
         extends BaseEntity {
