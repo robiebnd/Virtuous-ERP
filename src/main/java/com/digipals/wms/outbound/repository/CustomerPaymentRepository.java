785ff238ea1a0b51b93f6082650fe4f6a3f7b2fc
Robson Banda <138120031+robiebnd@users.noreply.github.com>
2026-09-04 11:17:19 +0200

    Remove obsolete legacy CustomerPayment repository

--- src/main/java/com/digipals/wms/outbound/repository/CustomerPaymentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.digipals.wms.outbound.repository;
-import com.digipals.wms.outbound.entity.CustomerPayment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.*;
-public interface CustomerPaymentRepository extends JpaRepository<CustomerPayment, UUID> {
-    Optional<CustomerPayment> findByPaymentNumber(String paymentNumber);
-}
