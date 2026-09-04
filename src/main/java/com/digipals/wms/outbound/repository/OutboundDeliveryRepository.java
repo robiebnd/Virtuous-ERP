f4a3560dea221c19f2fe29e548f330c8eec0660f
Robson Banda <138120031+robiebnd@users.noreply.github.com>
2026-09-04 08:14:13 +0200

    Remove obsolete legacy outbound delivery repository

--- src/main/java/com/digipals/wms/outbound/repository/OutboundDeliveryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.digipals.wms.outbound.repository;
-import com.digipals.wms.outbound.entity.OutboundDelivery;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.*;
-public interface OutboundDeliveryRepository extends JpaRepository<OutboundDelivery, UUID> {
-    Optional<OutboundDelivery> findByDeliveryNumber(String deliveryNumber);
-    List<OutboundDelivery> findBySalesOrderId(UUID salesOrderId);
-}
