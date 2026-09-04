ca244a27f64d34c0b5d8e546d532aea82156b538
Robson Banda <138120031+robiebnd@users.noreply.github.com>
2026-09-04 10:20:02 +0200

    Remove obsolete legacy customer invoice repository

--- src/main/java/com/digipals/wms/outbound/repository/CustomerInvoiceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.digipals.wms.outbound.repository;
-import com.digipals.wms.outbound.entity.CustomerInvoice;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.*;
-public interface CustomerInvoiceRepository extends JpaRepository<CustomerInvoice, UUID> {
-    Optional<CustomerInvoice> findByInvoiceNumber(String invoiceNumber);
-    List<CustomerInvoice> findByCustomerId(UUID customerId);
-}
