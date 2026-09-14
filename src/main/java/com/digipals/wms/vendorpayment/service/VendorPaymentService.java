23aac818222baf2f9526948484ceacf8d21f0c36
Robson Banda <138120031+robiebnd@users.noreply.github.com>
2026-09-14 11:57:01 +0200

    Add vendor payment service contract

---
+++ src/main/java/com/digipals/wms/vendorpayment/service/VendorPaymentService.java
@@ -0,0 +1,12 @@
+package com.digipals.wms.vendorpayment.service;
+
+import com.digipals.wms.vendorpayment.dto.CreateVendorPaymentRequest;
+import com.digipals.wms.vendorpayment.entity.VendorPayment;
+import java.util.List;
+import java.util.UUID;
+
+public interface VendorPaymentService {
+    VendorPayment create(CreateVendorPaymentRequest request);
+    VendorPayment approve(UUID id);
+    List<VendorPayment> findByInvoice(UUID invoiceId);
+}
