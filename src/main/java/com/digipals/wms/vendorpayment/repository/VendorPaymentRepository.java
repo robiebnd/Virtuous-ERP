cea81049a98a619d3e0d083a0e87150db2524452
Robson Banda <138120031+robiebnd@users.noreply.github.com>
2026-09-16 13:54:05 +0200

    Add vendor payment list repository query

--- src/main/java/com/digipals/wms/vendorpayment/repository/VendorPaymentRepository.java
+++ src/main/java/com/digipals/wms/vendorpayment/repository/VendorPaymentRepository.java
@@ -6,5 +6,6 @@ import java.util.List;
 import java.util.UUID;
 
 public interface VendorPaymentRepository extends JpaRepository<VendorPayment, UUID> {
+    List<VendorPayment> findAllByOrderByPaymentDateDesc();
     List<VendorPayment> findAllByVendorInvoiceIdOrderByPaymentDateDesc(UUID vendorInvoiceId);
 }
