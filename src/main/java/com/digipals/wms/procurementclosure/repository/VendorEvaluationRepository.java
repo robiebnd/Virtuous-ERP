30e30a12f94abd53bf1b512e3f0778e5bb5ef7a8
Robson Banda <138120031+robiebnd@users.noreply.github.com>
2026-09-17 08:52:07 +0200

    Fix duplicate VendorEvaluationRepository bean

--- src/main/java/com/digipals/wms/procurementclosure/repository/VendorEvaluationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.digipals.wms.procurementclosure.repository;
-import com.digipals.wms.procurementclosure.entity.VendorEvaluation;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.*;
-public interface VendorEvaluationRepository extends JpaRepository<VendorEvaluation, UUID> {
- List<VendorEvaluation> findBySupplierId(UUID supplierId);
- List<VendorEvaluation> findByPurchaseOrderId(UUID purchaseOrderId);
-}
