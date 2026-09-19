ca8b462afe77c6dfedc31c63148a0e3001bc54b5
Robson Banda <138120031+robiebnd@users.noreply.github.com>
2026-09-19 17:39:30 +0200

    Add treasury risk capability TreasuryRiskLimitRepository.java

---
+++ src/main/java/com/digipals/wms/finance/repository/TreasuryRiskLimitRepository.java
@@ -0,0 +1,1 @@
+package com.digipals.wms.finance.repository; import com.digipals.wms.finance.entity.*; import org.springframework.data.jpa.repository.JpaRepository; import java.util.*; public interface TreasuryRiskLimitRepository extends JpaRepository<TreasuryRiskLimit,UUID>{ Optional<TreasuryRiskLimit> findByLimitCode(String limitCode); }
