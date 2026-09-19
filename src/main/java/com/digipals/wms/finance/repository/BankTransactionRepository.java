fac323da662f0b8437c1e5b1e5fcabfd3065553f
Robson Banda <138120031+robiebnd@users.noreply.github.com>
2026-09-19 17:03:02 +0200

    Add bank transaction repository

---
+++ src/main/java/com/digipals/wms/finance/repository/BankTransactionRepository.java
@@ -0,0 +1,10 @@
+package com.digipals.wms.finance.repository;
+import com.digipals.wms.finance.entity.BankTransaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.LocalDate;
+import java.util.*;
+public interface BankTransactionRepository extends JpaRepository<BankTransaction,UUID>{
+ Optional<BankTransaction> findByBankAccountIdAndTransactionNumber(UUID bankAccountId,String transactionNumber);
+ List<BankTransaction> findByBankAccountIdOrderByTransactionDateDesc(UUID bankAccountId);
+ List<BankTransaction> findByStatusOrderByTransactionDateDesc(String status);
+}
