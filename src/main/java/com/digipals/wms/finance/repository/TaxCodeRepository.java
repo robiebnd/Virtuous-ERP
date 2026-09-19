9d3b2e5cc505e048442d5e8ef5c294e8358b535a
Robson Banda <138120031+robiebnd@users.noreply.github.com>
2026-09-20 00:22:43 +0200

    Make tax code lookups company aware

--- src/main/java/com/digipals/wms/finance/repository/TaxCodeRepository.java
+++ src/main/java/com/digipals/wms/finance/repository/TaxCodeRepository.java
@@ -2,4 +2,6 @@ package com.digipals.wms.finance.repository;
 import com.digipals.wms.finance.entity.TaxCode; import org.springframework.data.jpa.repository.JpaRepository; import java.util.*;
 public interface TaxCodeRepository extends JpaRepository<TaxCode,UUID> {
     Optional<TaxCode> findByTaxCode(String taxCode);
+    Optional<TaxCode> findByCompanyCodeAndTaxCodeIgnoreCase(String companyCode,String taxCode);
+    List<TaxCode> findByCompanyCodeOrderByTaxCode(String companyCode);
 }
