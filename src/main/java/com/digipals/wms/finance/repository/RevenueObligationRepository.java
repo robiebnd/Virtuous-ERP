18a671ef2b6840bd710277ae314fc1695ce1140e
Robson Banda <138120031+robiebnd@users.noreply.github.com>
2026-09-20 00:33:40 +0200

    Return all performance obligations for a revenue contract

--- src/main/java/com/digipals/wms/finance/repository/RevenueObligationRepository.java
+++ src/main/java/com/digipals/wms/finance/repository/RevenueObligationRepository.java
@@ -1,5 +1,5 @@
 package com.digipals.wms.finance.repository;
 import com.digipals.wms.finance.entity.RevenueObligation; import org.springframework.data.jpa.repository.JpaRepository; import java.util.*;
 public interface RevenueObligationRepository extends JpaRepository<RevenueObligation,UUID> {
-    Optional<RevenueObligation> findByContractId(UUID contractId);
+    List<RevenueObligation> findByContractId(UUID contractId);
 }
