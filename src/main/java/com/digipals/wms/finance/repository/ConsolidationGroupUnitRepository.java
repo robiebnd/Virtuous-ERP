54edb8fafb08e198445e33fdf6d169a9da753c46
Robson Banda <138120031+robiebnd@users.noreply.github.com>
2026-09-20 00:32:21 +0200

    Fix consolidation group unit repository key import

--- src/main/java/com/digipals/wms/finance/repository/ConsolidationGroupUnitRepository.java
+++ src/main/java/com/digipals/wms/finance/repository/ConsolidationGroupUnitRepository.java
@@ -1,2 +1,2 @@
 package com.digipals.wms.finance.repository;
-import com.digipals.wms.finance.entity.ConsolidationGroupUnit; import org.springframework.data.jpa.repository.JpaRepository; import java.util.*; public interface ConsolidationGroupUnitRepository extends JpaRepository<ConsolidationGroupUnit,ConsolidationGroupUnitId>{ List<ConsolidationGroupUnit> findByIdGroupId(UUID groupId); }
+import com.digipals.wms.finance.entity.ConsolidationGroupUnit; import com.digipals.wms.finance.entity.ConsolidationGroupUnitId; import org.springframework.data.jpa.repository.JpaRepository; import java.util.*; public interface ConsolidationGroupUnitRepository extends JpaRepository<ConsolidationGroupUnit,ConsolidationGroupUnitId>{ List<ConsolidationGroupUnit> findByIdGroupId(UUID groupId); }
