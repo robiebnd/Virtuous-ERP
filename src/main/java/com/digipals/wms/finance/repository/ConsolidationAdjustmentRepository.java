9071395ed1a8f46d946e59a1b778c257680a1061
Robson Banda <138120031+robiebnd@users.noreply.github.com>
2026-09-19 17:33:33 +0200

    Add consolidation and funds entity ConsolidationAdjustmentRepository.java

---
+++ src/main/java/com/digipals/wms/finance/repository/ConsolidationAdjustmentRepository.java
@@ -0,0 +1,1 @@
+package com.digipals.wms.finance.repository; import com.digipals.wms.finance.entity.*; import org.springframework.data.jpa.repository.JpaRepository; import java.util.*; public interface ConsolidationAdjustmentRepository extends JpaRepository<ConsolidationAdjustment,UUID>{ List<ConsolidationAdjustment> findByGroupIdOrderByPeriodStartDesc(UUID groupId); }
