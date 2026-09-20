365b9de8b307c0ebf69cfe535e5d0703fb5dd6a2
Robson Banda <138120031+robiebnd@users.noreply.github.com>
2026-09-20 12:40:32 +0200

    Add finance production repositories

---
+++ src/main/java/com/digipals/wms/finance/repository/CopaAllocationTargetRepository.java
@@ -0,0 +1,1 @@
+package com.digipals.wms.finance.repository; import com.digipals.wms.finance.entity.CopaAllocationTarget; import org.springframework.data.jpa.repository.JpaRepository; import java.util.*; public interface CopaAllocationTargetRepository extends JpaRepository<CopaAllocationTarget,UUID>{List<CopaAllocationTarget> findByRuleIdAndActiveTrue(UUID ruleId);}
